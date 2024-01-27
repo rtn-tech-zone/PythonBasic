9a9a974b1c89fcd58daa14ebfd5044c264d30b21
rtn-tech-zone <sandibnayak@gmai.com>
2024-01-27 17:54:29 +0530

    "Day1"

---
+++ LearningBasic/01_a_HelloWorld.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+
+print("Ticket Booking intitated.......................")
+#load page
+#Enter details of station
+print("Getting Train List............")
+#Select Train
+#select Class
+#Go for book
+#Select Pasenger
+print("Payment Started............")
+#Go for Payment
+#Enter Payment Details
+#Enter Pin/Otp
+#Payment Process and Done
+#Book Ticket
+#Get PNR
+#send the ticket sms to customer
+print("..............Ticket Booking Completed")
